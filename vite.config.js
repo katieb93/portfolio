190c4bdbdfad6dccd096e786036b14b7531f53ec
YOUR NAME <YOUR EMAIL>
2024-08-19 21:24:16 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/Users/katiebrown/portfolio-site/src/assets/NovelList.png',
+        // Add other externalized paths or modules here if needed
+      ],
+    },
+  },
 })
